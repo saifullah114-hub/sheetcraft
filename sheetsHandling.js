c3d3a879d4ed0d897cdbbfdc5802f25eb253d245
saifullah114-hub <saifu3785@gmail.com>
2023-09-01 01:24:16 +0530

    updated grid.js logic

--- sheetsHandling.js
+++ sheetsHandling.js
@@ -1,44 +1,139 @@
-let downloadBtn = document.querySelector(".download");
-let openBtn = document.querySelector(".open");
+let activeSheetColor  ="#ced6e0";
+let sheetsFolderCont = document.querySelector(".sheets-folder-cont");
+let addSheetBtn = document.querySelector(".sheet-add-icon");
 
-// Download Task
-downloadBtn.addEventListener("click", (e) => {
-    let jsonData = JSON.stringify([sheetDB, graphComponentMatrix]);
-    let file = new Blob([jsonData], { type: "application/json" });
+addSheetBtn.addEventListener("click", (e) => {
+    let sheet = document.createElement("div");
+    sheet.setAttribute("class", "sheet-folder");
 
-    let a = document.createElement("a");
-    a.href = URL.createObjectURL(file);
-    a.download = "SheetData.json";
-    a.click();
+    let allSheetFolders = document.querySelectorAll(".sheet-folder");
+    sheet.setAttribute("id", allSheetFolders.length);
+
+    sheet.innerHTML = `
+        <div class="sheet-content">Sheet ${allSheetFolders.length + 1}</div>
+    `;
+
+    sheetsFolderCont.appendChild(sheet);
+    sheet.scrollIntoView();
+
+    // DB
+    createSheetDB();
+    createGraphComponentMatrix();
+    handleSheetActiveness(sheet);
+    handleSheetRemoval(sheet);
+    sheet.click();
 })
 
-// Open task (upload)
-openBtn.addEventListener("click", (e) => {
-    // Opens file explorer
-    let input = document.createElement("input");
-    input.setAttribute("type", "file");
-    input.click();
+function handleSheetRemoval(sheet) {
+    sheet.addEventListener("mousedown", (e) => {
+        // Right click
+        if (e.button !== 2) return;
 
-    input.addEventListener("change", (e) => {
-        let fr = new FileReader();
-        let files = input.files;
-        let fileObj = files[0];
+        let allSheetFolders = document.querySelectorAll(".sheet-folder");
+        if (allSheetFolders.length === 1) {
+            alert("You need to have atleast one sheet!!");
+            return;
+        }
 
-        fr.readAsText(fileObj);
-        fr.addEventListener("load", (e) => {
-            let readSheetData = JSON.parse(fr.result);
+        let response = confirm("Your sheet will be removed permanently, Are you sure?");
+        if (response === false) return;
 
-            // Basic sheet with default data will be created
-            addSheetBtn.click();
+        let sheetIdx = Number(sheet.getAttribute("id"));
+        // DB
+        collectedSheetDB.splice(sheetIdx, 1);
+        collectedGraphComponent.splice(sheetIdx, 1);
+        // UI
+        handleSheetUIRemoval(sheet)
 
-            // SheetDB, graphComponent
-            sheetDB = readSheetData[0];
-            graphComponentMatrix = readSheetData[1];
-
-            collectedSheetDB[collectedSheetDB.length-1] = sheetDB;
-            collectedGraphComponent[collectedGraphComponent.length-1] = graphComponentMatrix;
-
-            handleSheetProperties();
-        })
+        // By default DB to sheet 1 (active)
+        sheetDB = collectedSheetDB[0];
+        graphComponentMatrix = collectedGraphComponent[0];
+        handleSheetProperties();
     })
-})
+}
+
+function handleSheetUIRemoval(sheet) {
+    sheet.remove();
+    let allSheetFolders = document.querySelectorAll(".sheet-folder");
+    for (let i = 0;i < allSheetFolders.length;i++) {
+        allSheetFolders[i].setAttribute("id", i);
+        let sheetContent = allSheetFolders[i].querySelector(".sheet-content");
+        sheetContent.innerText = `Sheet ${i+1}`;
+        allSheetFolders[i].style.backgroundColor = "transparent";
+    }
+
+    allSheetFolders[0].style.backgroundColor = activeSheetColor;
+}
+
+function handleSheetDB(sheetIdx) {
+    sheetDB = collectedSheetDB[sheetIdx];
+    graphComponentMatrix = collectedGraphComponent[sheetIdx];
+}
+
+function handleSheetProperties() {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            let cell = document.querySelector(`.cell[rid="${i}"][cid="${j}"]`);
+            cell.click();
+        }
+    }
+    // By default click on first cell via DOM
+    let firstCell = document.querySelector(".cell");
+    firstCell.click();
+}
+
+function handleSheetUI(sheet) {
+    let allSheetFolders = document.querySelectorAll(".sheet-folder");
+    for (let i = 0; i < allSheetFolders.length; i++) {
+        allSheetFolders[i].style.backgroundColor = "transparent";
+    }
+    sheet.style.backgroundColor = activeSheetColor;
+}
+
+function handleSheetActiveness(sheet) {
+    sheet.addEventListener("click", (e) => {
+        let sheetIdx = Number(sheet.getAttribute("id"));
+        handleSheetDB(sheetIdx);
+        handleSheetProperties();
+        handleSheetUI(sheet);
+        console.log(sheetDB);
+    })
+}
+
+function createSheetDB() {
+    let sheetDB = [];
+    for (let i = 0; i < rows; i++) {
+        let sheetRow = [];
+        for (let j = 0; j < cols; j++) {
+            let cellProp = {
+                bold: false,
+                italic: false,
+                underline: false,
+                alignment: "left",
+                fontFamily: "monospace",
+                fontSize: "14",
+                fontColor: "#000000",
+                BGcolor: "#000000",  // Just for indication purpose,
+                value: "",
+                formula: "",
+                children: [],
+            }
+            sheetRow.push(cellProp);
+        }
+        sheetDB.push(sheetRow);
+    }
+    collectedSheetDB.push(sheetDB);
+}
+
+function createGraphComponentMatrix() {
+    let graphComponentMatrix = [];
+    for (let i = 0; i < rows; i++) {
+        let row = [];
+        for (let j = 0; j < cols; j++) {
+            // Why array -> More than 1 child relation(dependency)
+            row.push([]);
+        }
+        graphComponentMatrix.push(row);
+    }
+    collectedGraphComponent.push(graphComponentMatrix);
+}
